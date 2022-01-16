1f72d89553fdab09f75296f1e32a957459b15081
ziadkhachfen <zkhachfen@gmail.com>
2022-01-16 22:36:00 +0100

    last commit

--- app.R
+++ app.R
@@ -19,14 +19,14 @@ shinyApp(
         menuItem("Production", tabName = "production", icon = icon("chart-line")))
     ),
     dashboardBody(
-      h1("Agrégats segmentés de consommation et production électriques au pas 1/2 h"),
-      strong("Les données publiées donnent une vision de la consommation d'électricité au pas 1/2 h des points de soutirage < 36kVA. La liste déroulante permet de sélectionner l'agrégat souhaité : total de l'énergie consommée, courbes de charges moyennes ou nombre de points de soutirage."),
+      h1("Agr?gats segment?s de consommation et production ?lectriques au pas 1/2 h"),
+      strong("Les donn?es publi?es donnent une vision de la consommation d'?lectricit? au pas 1/2 h des points de soutirage < 36kVA. La liste d?roulante permet de s?lectionner l'agr?gat souhait? : total de l'?nergie consomm?e, courbes de charges moyennes ou nombre de points de soutirage."),
       tabItems(
         tabItem("consommationinf36",align="center",
                 column(width = 6, align="center",
                        box(
                          title = "Filtres",
-                         em("Deux axes d'analyse sont disponibles (par profil ou plage de puissance) sélectionnables par un bouton."),
+                         em("Deux axes d'analyse sont disponibles (par profil ou plage de puissance) s?lectionnables par un bouton."),
                          status = "info",
                          solidHeader = TRUE,
                          width = 10,align="center",
@@ -36,21 +36,21 @@ shinyApp(
                          
                          
                          column(10,align="center",style=list("padding-right: 3px;"),
-                                selectInput(inputId = "Region_inf", label = "Sélectionner le national ou une région", choices= c(unique(data_inf_verticale$Region),"National") )),
+                                selectInput(inputId = "Region_inf", label = "S?lectionner le national ou une r?gion", choices= c(unique(data_inf_verticale$Region),"National", selected ='National') )),
                          
                          column(5,align="center",style=list("padding-right: 3px;"),
-                                selectInput(inputId = "Profil_inf", label = "Profil", choices= c(unique(data_inf_verticale$Profil),"Tous les profils" ))),
+                                selectInput(inputId = "Profil_inf", label = "Profil", choices= c(unique(data_inf_verticale$Profil),"Tous les profils", selected ='Tous les profils' ))),
                          
                          column(5,align="center",style=list("padding-right: 3px;"),
                                 selectInput(inputId = "plage_puissance_inf", label = "Plage de puissance souscrite", choices=unique(data_inf_verticale$Plage.de.puissance.souscrite))),
                          
                          column(10,align="center",style=list("padding-right: 3px;"),
-                                dateRangeInput(inputId = "dates_inf", label = "Période", start = "2021-06-01",end = "2021-09-30")), 
+                                dateRangeInput(inputId = "dates_inf", label = "P?riode", start = "2021-06-01",end = "2021-09-30")), 
                          
-                         #column(10,align="center",style=list("padding-right: 3px;"),
-                         
-                         #checkboxGroupInput(inputId = "pas", label = "Pas", choices = list("pas demi horaire", "pas quotidien"), selected = "pas demi horaire")
-                         #)
+                         column(10,align="center",style=list("padding-right: 3px;"),
+                                
+                                checkboxGroupInput(inputId = "pas", label = "Pas", choices = list("pas demi horaire", "pas quotidien"), selected = "pas demi horaire")
+                         )
                        )),
                 column(width=6,
                        box(
@@ -65,7 +65,7 @@ shinyApp(
                 column(width = 6, align="center",
                        box(
                          title = "Filtres",
-                         em("Deux axes d'analyse sont disponibles (par profil ou plage de puissance) sélectionnables par un bouton."),
+                         em("Deux axes d'analyse sont disponibles (par profil ou plage de puissance) s?lectionnables par un bouton."),
                          status = "info",
                          solidHeader = TRUE,
                          width = 10,align="center",
@@ -74,10 +74,10 @@ shinyApp(
                                 selectInput(inputId = "cat_sup", label = " ", choices= unique(data_sup_verticale$variable) )),
                          
                          column(10,align="center",style=list("padding-right: 3px;"),
-                                selectInput(inputId = "nat_reg_sup", label = "Selectionner le national ou une région", choices=c(unique(data_sup_verticale$Region), "National"))),
+                                selectInput(inputId = "nat_reg_sup", label = "Selectionner le national ou une r?gion", choices=c(unique(data_sup_verticale$Region), "National"))),
                          
                          column(4,align="center",style=list("padding-right: 3px;"),
-                                selectInput(inputId = "secteur_activite_sup", label = "Secteur d'activité", choices=c(unique(data_sup_verticale$Secteur.activite), "Tous les secteurs")  )),
+                                selectInput(inputId = "secteur_activite_sup", label = "Secteur d'activit?", choices=c(unique(data_sup_verticale$Secteur.activite), "Tous les secteurs")  )),
                          
                          column(4,align="center",style=list("padding-right: 3px;"),
                                 selectInput(inputId = "profil_sup", label = "Profil", choices= c( unique(data_sup_verticale$Profil), "Tous les profils"))),
@@ -88,7 +88,7 @@ shinyApp(
                          column(10,align="center",style=list("padding-right: 3px;"),
                                 dateRangeInput(
                                   inputId = "dates_sup",
-                                  label = "Période",
+                                  label = "P?riode",
                                   start = "2021-06-01",
                                   end = "2021-09-30")), 
                          column(10,align="center",style=list("padding-right: 3px;"),
@@ -109,7 +109,7 @@ shinyApp(
                 column(width = 6,align="center",
                        box(
                          title = "Filtres",
-                         em("Deux axes d'analyse sont disponibles (par profil ou plage de puissance) sélectionnables par un bouton."),
+                         em("Deux axes d'analyse sont disponibles (par profil ou plage de puissance) s?lectionnables par un bouton."),
                          status = "info",
                          solidHeader = TRUE,
                          width = 10,align="center",
@@ -118,10 +118,10 @@ shinyApp(
                                 selectInput(inputId = "cat_prod", label = " ", choices= unique(data_11_verticale$variable))),
                          
                          column(10,align="center",style=list("padding-right: 3px;"),          
-                                selectInput(inputId = "nat_reg_prod", label = "Sélectionner le national ou une région", choices= c(unique(data_11_verticale$Region), "National") )),
+                                selectInput(inputId = "nat_reg_prod", label = "S?lectionner le national ou une r?gion", choices= c(unique(data_11_verticale$Region), "National"), selected ='National' )),
                          
                          column(5,align="center",style=list("padding-right: 3px;"),           
-                                selectInput(inputId = "filiere_prod", label = "Filiere", choices= c(unique(data_11_verticale$Filiere.de.production), "Toutes les filieres" ) )),
+                                selectInput(inputId = "filiere_prod", label = "Filiere", choices= c(unique(data_11_verticale$Filiere.de.production), "Toutes les filieres" ), selected = "Toutes les filieres" )),
                          
                          column(5,align="center",style=list("padding-right: 3px;"),
                                 selectInput(inputId = "plage_puissance_prod", label = "Plage de puissance d'injection", choices= unique(data_11_verticale$Plage.de.puissance.injection))),
@@ -129,7 +129,7 @@ shinyApp(
                          column(10,align="center",style=list("padding-right: 3px;"),
                                 dateRangeInput(
                                   inputId = "dates_prod",
-                                  label = "Période",
+                                  label = "P?riode",
                                   start = "2021-06-01",
                                   end = "2021-09-30")),
                          column(10,align="center",style=list("padding-right: 3px;"),   
@@ -149,7 +149,7 @@ shinyApp(
                 
         )
       )),
-    title = "Analyse des consommations et productions régionales au pas demi horaire",
+    title = "Analyse des consommations et productions r?gionales au pas demi horaire",
     skin = "black"
   ),
   
@@ -169,7 +169,7 @@ shinyApp(
             geom_stream(type = "ridge") +
             theme(plot.title = element_text(hjust = 0.5)) +
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps ") + 
             theme(
@@ -184,7 +184,7 @@ shinyApp(
             geom_stream(type = "ridge") +
             theme(plot.title = element_text(hjust = 0.5)) +
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps ") + 
             theme(
@@ -202,7 +202,7 @@ shinyApp(
             geom_stream(type = "ridge") +
             theme(plot.title = element_text(hjust = 0.5)) +
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps ") + 
             theme(
@@ -217,7 +217,7 @@ shinyApp(
             geom_stream(type = "ridge") +
             theme(plot.title = element_text(hjust = 0.5)) +
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps ") + 
             theme(
@@ -234,14 +234,14 @@ shinyApp(
     construit_sup <- reactive({
       if (input$nat_reg_sup == "National")
       {
-        new_data1 <- data_sup_verticale  %>% filter(Secteur.activite == input$secteur_activite_sup, variable %in% input$cat_sup, Plage.de.puissance.souscrite == input$plage_puissance_sup, Horodate <= as.POSIXct(input$dates_sup[2]) & Horodate >= as.POSIXct(input$dates_sup[1])) %>% group_by(Horodate,  variable) %>% summarise(value = sum(value))
+        new_data1 <- data_sup_verticale  %>% filter(Secteur.activite == input$secteur_activite_sup,Profil %in% input$profil_sup, variable %in% input$cat_sup, Plage.de.puissance.souscrite == input$plage_puissance_sup, Horodate <= as.POSIXct(input$dates_sup[2]) & Horodate >= as.POSIXct(input$dates_sup[1])) %>% group_by(Horodate,  variable) %>% summarise(value = sum(value))
         
         
         ggplot(data = new_data1)+
           aes(x=as.POSIXct(Horodate), y=value , fill = Secteur.activite) +
           theme(plot.title = element_text(hjust = 0.5))+
           geom_stream(type = "ridge") + 
-          ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+          ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
           ylab(" ")+
           xlab("Temps (au pas jours)") + 
           theme(
@@ -261,7 +261,7 @@ shinyApp(
             aes(x=as.POSIXct(Horodate), y=value , fill = Secteur.activite) +
             theme(plot.title = element_text(hjust = 0.5))+
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps (au pas jours)") + 
             theme(
@@ -277,7 +277,7 @@ shinyApp(
             aes(x=as.POSIXct(Horodate), y=value , fill = Secteur.activite) +
             theme(plot.title = element_text(hjust = 0.5))+
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps (au pas jours)") + 
             theme(
@@ -304,7 +304,7 @@ shinyApp(
           ggplot(data= new_data3)+aes(x=as.POSIXct(Horodate), y=value , fill = Filiere.de.production) +
             theme(plot.title = element_text(hjust = 0.5))+
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps ") + 
             theme(
@@ -318,7 +318,7 @@ shinyApp(
           ggplot(data= new_data3)+aes(x=as.POSIXct(Horodate), y=value , fill = Filiere.de.production) +
             theme(plot.title = element_text(hjust = 0.5))+
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps ") + 
             theme(
@@ -335,7 +335,7 @@ shinyApp(
           ggplot(data= new_data3)+aes(x=as.POSIXct(Horodate), y=value , fill = Filiere.de.production) +
             theme(plot.title = element_text(hjust = 0.5))+
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps ") + 
             theme(
@@ -351,7 +351,7 @@ shinyApp(
             aes(x=as.POSIXct(Horodate), y=value , fill = Filiere.de.production) +
             theme(plot.title = element_text(hjust = 0.5))+
             geom_stream(type = "ridge") + 
-            ggtitle("Evolution de la quantité d'énergie soutirée moyenne (MWh)")+
+            ggtitle("Evolution de la quantit? d'?nergie soutir?e moyenne (MWh)")+
             ylab(" ")+
             xlab("Temps ") + 
             theme(
@@ -418,7 +418,7 @@ shinyApp(
       write.csv(df, "C:\\Users\\Goldenshop.ma\\Documents\\data_prod.csv")
     })
     
-    data3 <- eventReactive(input$buttonsup, {
+    data2 <- eventReactive(input$buttonsup, {
       df <- data.frame(donnes_prod)
       write.csv(df, "C:\\Users\\Goldenshop.ma\\Documents\\data_sup.csv")
     })
